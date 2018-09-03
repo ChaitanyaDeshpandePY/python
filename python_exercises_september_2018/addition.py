b71911bf32bd88781e95ca5395503b0bd1f329ba
ChaitanyaDeshpandePY <ch.deshpande@gmail.com>
2018-09-03 20:56:38 +0530

    Assignments

---
+++ python_exercises_september_2018/addition.py
@@ -0,0 +1,27 @@
+#!C:\Users\Internet\AppData\Local\Programs\Python\Python37\python.exe
+
+# Write a Program to accept the 2 numbers from the user and display their addition
+# Developed by : Chaitanya Deshpande
+# Date : 28-08-2018
+# Script Name : Addition.py
+
+ 
+
+# Program Begins
+# Accepting User Inputs
+num1 = input("Enter First number : ")
+num2 = input("Enter Second number : ")
+int(num1)
+int(num2)
+
+# Addition of 2 Numbers
+
+num3 = int(num1) + int(num2)
+
+# Printing the Addition of 2 Numbers
+
+print("Addition of 2 numbers is : ")
+
+print (int(num3))
+
+# Program Ends
---
+++ python_exercises_september_2018/power.py
@@ -0,0 +1,23 @@
+# Write a Program to accept 2 numbers from the User and display their Power
+# Developed by : Chaitanya Deshpande
+# Date : 28-08-2018
+# Script Name : Power.py
+
+ 
+
+# Program Begins
+# Accepting the User Inputs
+
+num1 = input("Enter first number : ")
+int(num1)
+num2 = input("Enter second number : ")
+int(num2)
+
+# Using ** Operator to generate the Power
+
+power = int(num1) ** int(num2)
+
+# Printing the Power
+print(int(power))
+
+# Program Ends

c929c53d8908eb2d6103975de22f7385f5a62b0a
ChaitanyaDeshpandePY <ch.deshpande@gmail.com>
2018-09-03 20:48:32 +0530

    First Home Work Program

---
+++ python_exercises_september_2018/hello_user.py
@@ -0,0 +1,19 @@
+#! C:\Users\Internet\AppData\Local\Programs\Python\Python37\python.exe
+
+# Write a Python Program to Accept a name from user and greet him with the message “Hello <name>”
+# Developed by : Chaitanya Deshpande
+# Date : 28-08-2018
+# Script Name : Hello.py
+
+ 
+
+# Program Begins
+# Accepting the User Input
+
+name = input("Enter User Input : ")
+str(name)
+
+# Printing / Greeting with Input
+print("Hello" + " " + str(name))
+
+# Program Ends
